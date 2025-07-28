c477422aa3877d559b424e43cedb4c9f7e57ba5d
Jordan Suber <jordansuber@gmail.com>
2025-07-28 15:56:47 -0400

    blah

--- environments/dev/helm.tf
+++ environments/dev/helm.tf
@@ -29,18 +29,34 @@ provider "helm" {
 resource "helm_release" "react_app" {
   name       = "react-app"
   namespace  = "default"
+  chart      = "${path.module}/../../chart"
+  depends_on = [ module.eks ]
 
-  chart = "${path.module}/../../chart"   # local chart directory
-
-  # Only the things that differ from values.yaml
-  set = [
-    {
-      name  = "image.repository"
-      value = "866934333672.dkr.ecr.us-east-1.amazonaws.com/jordansuber/react-app"
-    },
-    {
-      name  = "image.tag"
-      value = "0.0.3"
-    }
+  values = [
+    yamlencode({
+      image = {
+        repository = "866934333672.dkr.ecr.us-east-1.amazonaws.com/jordansuber/react-app"
+        tag        = "0.0.3"
+      }
+      ingress = {
+        enabled          = true
+        ingressClassName = "alb"
+        annotations = {
+          "alb.ingress.kubernetes.io/scheme"      = "internet-facing"
+          "alb.ingress.kubernetes.io/target-type" = "ip"
+        }
+        hosts = [
+          {
+            host = "app.${var.zone_name}"
+            paths = [
+              {
+                path     = "/"
+                pathType = "Prefix"
+              }
+            ]
+          }
+        ]
+      }
+    })
   ]
 }
